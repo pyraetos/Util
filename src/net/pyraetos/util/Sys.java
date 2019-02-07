baeb54eaa22156f3b5d050770aa2bbbcb058e291
Joe <joejohnson7371@gmail.com>
2019-02-06 21:08:40 -0600

    w
    Signed-off-by: Joe <joejohnson7371@gmail.com>

--- src/net/pyraetos/util/Sys.java
+++ src/net/pyraetos/util/Sys.java
@@ -126,6 +126,22 @@ public abstract class Sys{
 		return true;
 	}
 	
+	public static int min(int... arr) {
+		int min = Integer.MAX_VALUE;
+		for(int i : arr) {
+			if(i < min) min = i;
+		}
+		return min;
+	}
+	
+	public static int max(int... arr) {
+		int max = Integer.MIN_VALUE;
+		for(int i : arr) {
+			if(i > max) max = i;
+		}
+		return max;
+	}
+	
 	public static long time(){
 		return System.currentTimeMillis();
 	}
